47e8aa36d5af826fc46d9b5bd9fd98f1ce203d9d
matsu129 <78124432+matsu129@users.noreply.github.com>
2025-06-09 19:56:52 -0700

    add food options

--- JavaScript/bottom.js
+++ JavaScript/bottom.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const foodModal = document.getElementById('foodModal');
+  const closeModal = document.getElementById('closeModal');
+  const foodChoices = document.querySelectorAll('.food-choice');
+
   document.querySelectorAll('#action button').forEach(button => {
     button.addEventListener('click', () => {
       const action = button.dataset.action;
@@ -6,14 +10,34 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
+  if (closeModal) {
+    closeModal.addEventListener('click', () =>{
+      foodModal.classList.add('hidden');
+    })
+  }
+
   function handleAction(action) {
     switch (action) {
       case 'eat':
-        // #food（opacity: 1 -> 0）
-        document.querySelector('#food').style.opacity = '1';
-        setTimeout(() => {
-          document.querySelector('#food').style.opacity = '0';
-        }, 3000);
+        // #foodModal show
+        foodModal.classList.remove('hidden');
+        foodChoices.forEach(choice => {
+          choice.addEventListener('click', () => {
+            const food = choice.dataset.food;
+            console.log(choice.dataset.food);
+            // close Modal
+            foodModal.classList.add('hidden');
+            // show the food
+            foodDisplay.textContent = food;
+            foodDisplay.classList.remove('hidden');
+            foodDisplay.classList.add('show');
+            // 
+            setTimeout(() => {
+              foodDisplay.classList.remove('show');
+              foodDisplay.classList.add('hidden');
+            }, 3000);
+          })
+        })
         break;
       case 'sleep':
           document.querySelector('#piyo').src = './assets/suibun.png';
