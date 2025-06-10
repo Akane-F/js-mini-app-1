d7a54efd09aa5b888c67a0dfa4faff9c77247ddf
Akane-F <akanefukuda.26@gmail.com>
2025-06-09 19:31:24 -0700

    Add gauge changing logic

--- JavaScript/bottom.js
+++ JavaScript/bottom.js
@@ -1,3 +1,60 @@
+// ゲージのステータス管理
+let gauge = {
+  energy: 5,
+  happiness: 5,
+};
+
+function increaseEnergy() {
+  if (gauge.energy < 5) {
+    gauge.energy++;
+    updateGauge('energy', gauge.energy, true);
+  }
+}
+
+function decreaseEnergy() {
+  if (gauge.energy > 0) {
+    gauge.energy--;
+    updateGauge('energy', gauge.energy, false);
+  }
+}
+
+function increaseHappiness() {
+  if (gauge.happiness < 5) {
+    gauge.happiness++;
+    updateGauge('happiness', gauge.happiness, tru);
+  }
+}
+
+function decreaseHappiness() {
+  if (gauge.happiness > 0) {
+    gauge.happiness--;
+    updateGauge('happiness', gauge.happiness, false);
+  }
+}
+
+function updateGauge(id, gauge, increase) {
+  const gaugeIcons = document.querySelectorAll(`#${id} .gauge-fg img`);
+  if (!gaugeIcons || gaugeIcons.length === 0) return;
+
+  const icon = increase ? gaugeIcons[gauge - 1] : gaugeIcons[gauge];
+  if (!icon) return;
+
+  icon.style.transition = "opacity 1s ease";
+
+  if (increase && gauge > 0) {
+    icon.style.opacity = 1;
+  } else if (!increase && gauge < gaugeIcons.length ) {
+    icon.style.opacity = 0;
+  }
+}
+
+// 3分ごとに、energy:-1 happiness:-1
+setInterval(() => {
+  decreaseEnergy();
+  decreaseHappiness();
+}, 180000);
+
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('#action button').forEach(button => {
     button.addEventListener('click', () => {
@@ -9,56 +66,89 @@ document.addEventListener('DOMContentLoaded', () => {
   function handleAction(action) {
     switch (action) {
       case 'eat':
-        // #food（opacity: 1 -> 0）
+        // #food（opacity: 1 -> 0） energy:+1
         document.querySelector('#food').style.opacity = '1';
+        increaseEnergy();
         setTimeout(() => {
           document.querySelector('#food').style.opacity = '0';
         }, 3000);
         break;
       case 'sleep':
+          // energy:+1
           document.querySelector('#piyo').src = './assets/suibun.png';
           document.querySelector('#moon').style.opacity = '1';
+          increaseEnergy();
           setTimeout(() => {
             document.querySelector('#piyo').src = './assets/piyopiyo.gif';
             document.querySelector('#moon').style.opacity = '0';
           }, 3000);
         break;
       case 'game':
+        // energy:-1 happiness:+1
         document.querySelector('#piyo').src = './assets/mizudeppou.png';
+        decreaseEnergy();
+        increaseHappiness();
         setTimeout(() => {
+          if (gauge.energy == 0) {
+            document.querySelector('#piyo').src = './assets/sick-piyo.png';
+          } else {
             document.querySelector('#piyo').src = './assets/piyopiyo.gif';
-          }, 3000);
+          }
+        }, 3000);
         break;
       case 'medicine':
+        // energy:+1
         document.querySelector('#piyo').src = './assets/megusuri-shippai.png';
+        increaseEnergy();
         setTimeout(() => {
             document.querySelector('#piyo').src = './assets/piyopiyo.gif';
           }, 3000);
         break;
       case 'clean':
-        // #clean（opacity: 0 -> 1）
+        // #clean（opacity: 0 -> 1） happiness:+1 or happiness:-1
         document.querySelector('#poop').style.opacity = '0';
+        increaseHappiness();
         setTimeout(() => {
           document.querySelector('#poop').style.opacity = '1';
+          decreaseHappiness();
+          if (gauge.happiness == 0) {
+            document.querySelector('#piyo').src = './assets/boring-piyo.png';
+          }
         }, 300000)
         break;
       case 'scale':
+        // happiness:+1
         document.querySelector('#piyo').src = './assets/taiju.png';
+        increaseHappiness();
         setTimeout(() => {
             document.querySelector('#piyo').src = './assets/piyopiyo.gif';
           }, 3000);
         break;
       case 'education':
+        // energy:-1 happiness:+1
         document.querySelector('#piyo').src = './assets/study-piyoyo.png';
+        decreaseEnergy();
+        increaseHappiness();
         setTimeout(() => {
+          if (gauge.energy == 0) {
+            document.querySelector('#piyo').src = './assets/sick-piyo.png';
+          } else {
             document.querySelector('#piyo').src = './assets/piyopiyo.gif';
-          }, 3000);
+          }
+        }, 3000);
         break;
       case 'conversation':
+        // energy:-1 happiness:+1
         document.querySelector('#piyo').src = './assets/missetu.png';
+        decreaseEnergy();
+        increaseHappiness();
         setTimeout(() => {
+          if (gauge.energy == 0) {
+            document.querySelector('#piyo').src = './assets/sick-piyo.png';
+          } else {
             document.querySelector('#piyo').src = './assets/piyopiyo.gif';
-          }, 3000);
+          }
+        }, 3000);
         break;
     }
   }
